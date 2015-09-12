a31721f8e3ec4417afc9891cedb93d5c6d8a533a
Mavis Ou <mmmavisss@gmail.com>
2015-09-11 23:08:49 -0700

    post to Github periodically (with delay in between)

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-var _ = require("underscore");
 var express = require("express");
 var app = express();
-var request = require("request");
 var chalk = require("chalk");
 var GoogleSpreadsheet = require("google-spreadsheet");
 var generateIssue = require('./generate-github-issue');
+var githubRequest = require("./github-request");
 
 var Habitat = require("habitat");
 Habitat.load(".env");
@@ -13,6 +12,9 @@ var env = new Habitat("", {
 });
 
 var GITHUB_API_ENDPOINT = "https://api.github.com";
+var ROW_NUMBER_TO_START = 2; // this is the row # you want to fetch proposal data from. e.g., 2 means you want to fetch data from the first submitted proposal (Row#2)  
+var TOTAL_ROWS_TO_FETCH = 3;
+var POST_TO_GITHUB_DELAY_SECS = 3;
 
 app.set("port", env.get("port"));
 
@@ -20,61 +22,101 @@ app.get("/", function(req, res) {
   res.send("Hello World :D");
 });
 
-app.get("/migrate-issues", function(req, res) {
-  var numMigrated = 0;
-  var numFailed = 0;
-  var numIgnored = 0;
-  var rowsFailed = [];
-  var rowsIgnored = [];
-  var options = {
-    start: 426, // starts from Row x+1
-    num: 25
-  };
+var allProposals = [];
+var numProposals = 0;
+var numFetched = 0;
+var fetchOptions = {
+  start: ROW_NUMBER_TO_START-1, // minus 1 to offset the GoogleSpreadsheet default config, do not chnage this
+  num: TOTAL_ROWS_TO_FETCH
+};
+var postLog = {
+  numMigrated: 0,
+  numFailed: 0,
+  numIgnored: 0,
+  rowsFailed: [],
+  rowsIgnored: []
+};
 
-  fetchDataFromSpreadsheet(env.get("GOOGLE_SPREADSHEET_ID"), options, function(rows) {
-    res.send("Visit https://github.com/" + env.get("GITHUB_REPO") + "/issues" + " to see your migrated issues." );
+fetchDataFromSpreadsheet(env.get("GOOGLE_SPREADSHEET_ID"), fetchOptions, function(rows) {
+  allProposals = rows;
+  numProposals = allProposals.length;
+  postToGitHubWithDelay();
+});
 
-    rows.forEach(function(row, i) {
-      var rowNum = options.start + i + 1; // proposal data starts from row 1, not row 0
-      if (row.dontmigrate) {
-        numIgnored++;
-        rowsIgnored.push(rowNum);
-        console.log(chalk.yellow("Row #" + rowNum + " ignored\n"));
+function postToGitHubWithDelay() {
+  setTimeout(function(){
+    var proposal = allProposals[0+numFetched];
+    var rowNum = numFetched + 2;
+    if ( numFetched < numProposals ){
+      if (proposal.dontmigrate) {
+        postLog.numIgnored++;
+        postLog.rowsIgnored.push(rowNum);
+        console.log(chalk.yellow("Row #" + rowNum + " was ignored"));
+        numFetched++;
+        printCurrentReport();
+        postToGitHubWithDelay();
       } else {
-        // console.log(row.title);
-        // console.log(rowNum);
-        // console.log(row.sessionname + "\n");
-
-        postIssue( generateIssue(row, rowNum), function(error, successMsg) {
+        postIssue( generateIssue(proposal, rowNum), function(error, successMsg) {
           if (error) {
             console.log(chalk.red(error));
-            numFailed++;
-            rowsFailed.push(rowNum);
+            postLog.numFailed++;
+            postLog.rowsFailed.push(rowNum);
           } else {
             console.log(chalk.green(successMsg));
-            numMigrated++;
+            postLog.numMigrated++;
           }
-
-          console.log("  numMigrated: ", numMigrated);
-          console.log("  numFailed: ", numFailed);
-          console.log("  numIgnored: ", numIgnored);
+          numFetched++;
+          printCurrentReport();
 
           // print out the final result
-          if ( (numMigrated+numFailed+numIgnored) == options.num ) {
-            console.log(chalk.bgMagenta.bold("\n\n///// Batch Posting Done /////"));
-            console.log("Starts from Rows #" + (options.start+1));
-            console.log("Total rows fetched: " + options.num);
-            console.log("  numMigrated: " + numMigrated );
-            console.log("  numFailed: " + numFailed + " (Rows #" + rowsFailed.join(", ") + ")");
-            console.log("  numIgnored: " + numIgnored + " (Rows #" + rowsIgnored.join(", ") + ")");
-            console.log(chalk.bgMagenta.bold("//////////////////////////////"));
-            console.log("\n\n");
+          if ( (postLog.numMigrated+postLog.numFailed+postLog.numIgnored) == TOTAL_ROWS_TO_FETCH ) {
+            printFinalReport();
           }
+
+          postToGitHubWithDelay();
         });
       }
-    })
-  });
-});
+    } else {
+      console.log("/// ELSE ///");
+    }
+  }, POST_TO_GITHUB_DELAY_SECS*1000);
+}
+
+function printCurrentReport() {
+  console.log("numFetched = ", numFetched);
+  console.log("  numMigrated: ", postLog.numMigrated);
+  console.log("  numFailed: ", postLog.numFailed);
+  console.log("  numIgnored: ", postLog.numIgnored);
+  console.log("\n");
+}
+
+function printFinalReport() {
+  console.log(chalk.bgMagenta.bold("\n\n////////// Batch Posting Done //////////"));
+  console.log(chalk.magenta(
+    "Starts from Rows #" + ROW_NUMBER_TO_START + "\n" +
+    "Total Rows Fetched: " + numFetched + " (Expected: " + TOTAL_ROWS_TO_FETCH + ")" + "\n" +
+    "  numMigrated: " + postLog.numMigrated + "\n" +
+    "  numFailed: " + postLog.numFailed + " (Rows #" + postLog.rowsFailed.join(", ") + ")" + "\n" +
+    "  numIgnored: " + postLog.numIgnored + " (Rows #" + postLog.rowsIgnored.join(", ") + ")"
+  ));
+  console.log(chalk.bgMagenta.bold("////////////////////////////////////////"));
+  console.log("\n\n");
+}
+
+// function printProposal() {
+//   setTimeout(function(){
+//     if ( numFetched < numProposals ){
+//       var currentProposal = allProposals[0+numFetched];
+//       console.log(currentProposal.sessionname);
+//       printProposal();
+//       numFetched++;
+//       console.log("numFetched = ", numFetched);
+//       console.log("\n");
+//     } else {
+//       console.log("/// ELSE ///");
+//     }
+//   }, POST_TO_GITHUB_DELAY_SECS*1000);
+// }
 
 function fetchDataFromSpreadsheet(spreadsheetID, options, cb) {
   var my_sheet = new GoogleSpreadsheet(spreadsheetID);
@@ -85,14 +127,20 @@ function fetchDataFromSpreadsheet(spreadsheetID, options, cb) {
 }
 
 function postIssue(issue, cb) {
-  githubRequest({
+  var options = {
     method: "POST",
     url: GITHUB_API_ENDPOINT + "/repos/" + env.get("GITHUB_REPO") + "/issues",
     body: {
       title: issue.title,
       body: issue.body
     }
-  }, function(error, response, body) {
+  };
+  var userCreds = {
+    username: env.get("GITHUB_USERNAME"),
+    password: env.get("GITHUB_PASSWORD")
+  };
+
+  githubRequest(options, userCreds, function(error, response, body) {
     if (error) {
       cb(err);
     } 
@@ -100,26 +148,12 @@ function postIssue(issue, cb) {
     if (response.statusCode != 200 && response.statusCode != 201) {
       cb(new Error("Response status HTTP " + response.statusCode + ", Github error message: " + response.body.message));
     } else {
-      cb(null, "\nSuccessfully migrated '" + issue.title + "' (Issue #" + body.number + ")");
+      cb(null, "Successfully migrated '" + issue.title + "' (Issue #" + body.number + ")");
     }
     // console.log("\n\n response", response);
   });
 }
 
-function githubRequest(options, cb) {
-  request(_.extend({
-    headers: {
-      Accept: "application/vnd.github.v3+json",
-      "User-Agent": "testing MozFest proposal migrators"
-    },
-    auth: {
-      user: env.get("GITHUB_USERNAME"),
-      pass: env.get("GITHUB_PASSWORD")
-    },
-    json: true
-  }, options), cb);
-}
-
 app.listen(app.get("port"), function() {
-  console.log("Server listening on port %d...\n", app.get("port"));
+  console.log(chalk.cyan("Server listening on port %d...\n"), app.get("port"));
 });
