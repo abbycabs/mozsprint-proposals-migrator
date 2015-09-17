5fd03257b1e0292b1cfe685cf96c0be1cb7207cf
Mavis Ou <mmmavisss@gmail.com>
2015-09-17 15:12:08 -0700

    write report to a log file

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require("express");
 var app = express();
+var mkdirp = require("mkdirp");
+var fs = require("fs");
 var chalk = require("chalk");
 var GoogleSpreadsheet = require("google-spreadsheet");
-var generateIssue = require('./generate-github-issue');
+var moment = require("moment");
+var generateIssue = require("./generate-github-issue");
 var githubRequest = require("./github-request");
 
 var Habitat = require("habitat");
@@ -12,9 +15,10 @@ var env = new Habitat("", {
 });
 
 var GITHUB_API_ENDPOINT = "https://api.github.com";
-var ROW_NUMBER_TO_START = 2; // this is the row # you want to fetch proposal data from. e.g., 2 means you want to fetch data from the first submitted proposal (Row#2)  
-var TOTAL_ROWS_TO_FETCH = 3;
+var ROW_NUMBER_TO_START = 451; // this is the row # you want to fetch proposal data from. e.g., 2 means you want to fetch data from the first submitted proposal (Row#2)  
+var TOTAL_ROWS_TO_FETCH = 44;
 var POST_TO_GITHUB_DELAY_SECS = 3;
+var LOG_DIR_PATH = "./export-log";
 
 app.set("port", env.get("port"));
 
@@ -46,7 +50,7 @@ fetchDataFromSpreadsheet(env.get("GOOGLE_SPREADSHEET_ID"), fetchOptions, functio
 function postToGitHubWithDelay() {
   setTimeout(function(){
     var proposal = allProposals[0+numFetched];
-    var rowNum = numFetched + 2;
+    var rowNum = ROW_NUMBER_TO_START + numFetched;
     if ( numFetched < numProposals ){
       if (proposal.dontmigrate) {
         postLog.numIgnored++;
@@ -70,57 +74,70 @@ function postToGitHubWithDelay() {
 
           // print out the final result
           if ( (postLog.numMigrated+postLog.numFailed+postLog.numIgnored) == TOTAL_ROWS_TO_FETCH ) {
-            printFinalReport();
+            var timestamp = moment(Date.now()).format("YYYYMMD-hh.mm.ssA");
+            printFinalReport( generateFinalReport(timestamp) );
+            writeToLogFile( generateFinalReport(timestamp) );
           }
 
           postToGitHubWithDelay();
         });
       }
     } else {
+      console.log("/// DONE ///");
+    }
+  }, POST_TO_GITHUB_DELAY_SECS*1000);
+}
+
+function generateCurrentReport() {
+  return  "numFetched = " + numFetched + "\n" +
+          "  numMigrated: " + postLog.numMigrated + "\n" +
+          "  numFailed: " + postLog.numFailed + "\n" +
+          "  numIgnored: " + postLog.numIgnored + "\n";
+}
+
+function generateFinalReport(timestamp) {
+  return  {
+    header: "\n\n////////// Batch Posting Done //////////",
+    timestamp: timestamp,
+    detail: "Starts from Rows #" + ROW_NUMBER_TO_START + "\n" +
+            "Total Rows Fetched: " + numFetched + " (Expected: " + TOTAL_ROWS_TO_FETCH + ")" + "\n" +
+            "  numMigrated: " + postLog.numMigrated + "\n" +
+            "  numFailed: " + postLog.numFailed + " (Rows #" + postLog.rowsFailed.join(", ") + ")" + "\n" +
+            "  numIgnored: " + postLog.numIgnored + " (Rows #" + postLog.rowsIgnored.join(", ") + ")",
+    footer: "////////////////////////////////////////\n\n"
+  }
+}
+
+function printCurrentReport() {
+  console.log(generateCurrentReport());
+}
+
+function printFinalReport(finalReport) {
+  console.log( chalk.bgMagenta.bold(finalReport.header) );
+  console.log( chalk.magenta(finalReport.timestamp) );
+  console.log( "--------------------");
+  console.log( chalk.magenta(finalReport.detail) );
+  console.log( chalk.bgMagenta.bold(finalReport.footer) );
+}
+
+function printProposal() {
+  setTimeout(function(){
+    if ( numFetched < numProposals ){
+      var currentProposal = allProposals[0+numFetched];
+      console.log(currentProposal.sessionname);
+      printProposal();
+      numFetched++;
+      console.log("numFetched = ", numFetched);
+      console.log("\n");
+    } else {
       console.log("/// ELSE ///");
     }
   }, POST_TO_GITHUB_DELAY_SECS*1000);
 }
 
-function printCurrentReport() {
-  console.log("numFetched = ", numFetched);
-  console.log("  numMigrated: ", postLog.numMigrated);
-  console.log("  numFailed: ", postLog.numFailed);
-  console.log("  numIgnored: ", postLog.numIgnored);
-  console.log("\n");
-}
-
-function printFinalReport() {
-  console.log(chalk.bgMagenta.bold("\n\n////////// Batch Posting Done //////////"));
-  console.log(chalk.magenta(
-    "Starts from Rows #" + ROW_NUMBER_TO_START + "\n" +
-    "Total Rows Fetched: " + numFetched + " (Expected: " + TOTAL_ROWS_TO_FETCH + ")" + "\n" +
-    "  numMigrated: " + postLog.numMigrated + "\n" +
-    "  numFailed: " + postLog.numFailed + " (Rows #" + postLog.rowsFailed.join(", ") + ")" + "\n" +
-    "  numIgnored: " + postLog.numIgnored + " (Rows #" + postLog.rowsIgnored.join(", ") + ")"
-  ));
-  console.log(chalk.bgMagenta.bold("////////////////////////////////////////"));
-  console.log("\n\n");
-}
-
-// function printProposal() {
-//   setTimeout(function(){
-//     if ( numFetched < numProposals ){
-//       var currentProposal = allProposals[0+numFetched];
-//       console.log(currentProposal.sessionname);
-//       printProposal();
-//       numFetched++;
-//       console.log("numFetched = ", numFetched);
-//       console.log("\n");
-//     } else {
-//       console.log("/// ELSE ///");
-//     }
-//   }, POST_TO_GITHUB_DELAY_SECS*1000);
-// }
-
 function fetchDataFromSpreadsheet(spreadsheetID, options, cb) {
   var my_sheet = new GoogleSpreadsheet(spreadsheetID);
-  my_sheet.getRows(1, options, function(err, rows){
+  my_sheet.getRows(2, options, function(err, rows){
     // console.log(Object.keys(rows[0]));
     cb(rows);
   })
@@ -153,6 +170,29 @@ function postIssue(issue, cb) {
     // console.log("\n\n response", response);
   });
 }
+
+function writeToLogFile(finalReport) {
+  var fileContent = finalReport.header + "\n" +
+                    finalReport.timestamp + "\n" +
+                    finalReport.detail + "\n" +
+                    finalReport.footer;
+
+  mkdirp(LOG_DIR_PATH, function (err) {
+    if (err) {
+      console.error(err);
+    }
+    else {
+      var filePath = LOG_DIR_PATH + "/" + finalReport.timestamp + ".txt";
+      fs.writeFile(filePath, fileContent, function(err) {
+        if(err) {
+          console.log(err);
+        }
+        console.log(filePath + " was saved!");
+      }); 
+    }
+  });
+}
+
 
 app.listen(app.get("port"), function() {
   console.log(chalk.cyan("Server listening on port %d...\n"), app.get("port"));
